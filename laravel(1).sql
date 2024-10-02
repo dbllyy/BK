c7982977fc8370e8722148975cf870746d74cc43
dbllyy <debyli271@gmail.com>
2024-10-02 14:51:18 +0700

    update db

--- laravel(1).sql
+++ laravel(1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 01, 2024 at 11:02 AM
+-- Generation Time: Oct 02, 2024 at 09:50 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -28,8 +28,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cabang` (
-  `id_cabang` bigint(20) UNSIGNED NOT NULL,
-  `nama_cabang` varchar(255) NOT NULL,
+  `No_Cabang` bigint(20) UNSIGNED NOT NULL,
+  `Nama_Cabang` varchar(255) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -112,24 +112,6 @@ CREATE TABLE `job_batches` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `komputer`
---
-
-CREATE TABLE `komputer` (
-  `id_komputer` bigint(20) UNSIGNED NOT NULL,
-  `id_cabang` bigint(20) UNSIGNED NOT NULL,
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `merek` varchar(255) NOT NULL,
-  `kondisi` enum('baru','second') NOT NULL,
-  `keterangan` text DEFAULT NULL,
-  `tipe_service` enum('hardware','software','jaringan') NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `migrations`
 --
 
@@ -147,19 +129,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '0001_01_01_000000_create_users_table', 1),
 (2, '0001_01_01_000001_create_cache_table', 1),
 (3, '0001_01_01_000002_create_jobs_table', 1),
-(5, '2024_10_01_034816_create_cabang_table', 2);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `password_reset_tokens`
---
-
-CREATE TABLE `password_reset_tokens` (
-  `email` varchar(255) NOT NULL,
-  `token` varchar(255) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+(4, '2024_10_02_071635_create_cabang_table', 2);
 
 -- --------------------------------------------------------
 
@@ -181,7 +151,7 @@ CREATE TABLE `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('wJS6Pq9iMrXWRUVXiXtrTAl8TcdwPItOv8CqvKb0', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0', 'YTo1OntzOjY6Il90b2tlbiI7czo0MDoiSU1MZVRtamNveUlGUUxMVWlQSTN6bGRXa2s3V09YajVsYldDZmpLeSI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzA6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9zZXJ2aWNlcyI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6MzoidXJsIjthOjA6e31zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO30=', 1727773290);
+('cg09o2shD2gocWsNi9D8WXu0bAwNX38AL9G8pPzO', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiZ2pqWnF1VzZWVEtOVklQRnZvUDlxOEtFVzdzYUZLbVpPQlUxeUZKNCI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzA6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9rb21wdXRlciI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6MzoidXJsIjthOjE6e3M6ODoiaW50ZW5kZWQiO3M6MjY6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC91c2VyIjt9fQ==', 1727853318);
 
 -- --------------------------------------------------------
 
@@ -190,13 +160,10 @@ INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, 
 --
 
 CREATE TABLE `users` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `NIP` varchar(255) NOT NULL,
+  `Nama_Staff` varchar(255) NOT NULL,
+  `Role` enum('admin','staff') NOT NULL,
   `password` varchar(255) NOT NULL,
-  `role` enum('admin','staff','user') NOT NULL DEFAULT 'user',
-  `remember_token` varchar(100) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -205,8 +172,9 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `role`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'admin', 'admin@example.com', '2024-09-30 21:05:37', '$2y$12$VzPGL5fAruqVquxBdfwwZOkY9qOFaVga3s3uY4KanUYmj1FhOMZeO', 'admin', NULL, '2024-09-30 21:05:39', '2024-09-30 21:05:39');
+INSERT INTO `users` (`NIP`, `Nama_Staff`, `Role`, `password`, `created_at`, `updated_at`) VALUES
+('123456789', 'admin', 'admin', '$2y$12$pZIOsMYm/jjUJ27t8O37ruSuLoslqbpCZoEa4W8a/SfPBGieDCLuW', '2024-10-01 21:12:56', '2024-10-01 21:12:56'),
+('987654321', 'staff', 'staff', '$2y$12$hFSrVDRTgKvDbPQw3N3zou/P6jc.3Qg6jckSjHhS40hSSBHYHifdS', '2024-10-01 21:12:56', '2024-10-01 21:12:56');
 
 --
 -- Indexes for dumped tables
@@ -216,7 +184,7 @@ INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `ro
 -- Indexes for table `cabang`
 --
 ALTER TABLE `cabang`
-  ADD PRIMARY KEY (`id_cabang`);
+  ADD PRIMARY KEY (`No_Cabang`);
 
 --
 -- Indexes for table `cache`
@@ -251,37 +219,16 @@ ALTER TABLE `job_batches`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `komputer`
---
-ALTER TABLE `komputer`
-  ADD PRIMARY KEY (`id_komputer`);
-
---
 -- Indexes for table `migrations`
 --
 ALTER TABLE `migrations`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `password_reset_tokens`
---
-ALTER TABLE `password_reset_tokens`
-  ADD PRIMARY KEY (`email`);
-
---
--- Indexes for table `sessions`
---
-ALTER TABLE `sessions`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `1` (`user_id`),
-  ADD KEY `sessions_last_activity_index` (`last_activity`);
-
---
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
+  ADD PRIMARY KEY (`NIP`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -291,7 +238,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cabang`
 --
 ALTER TABLE `cabang`
-  MODIFY `id_cabang` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `No_Cabang` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -306,32 +253,10 @@ ALTER TABLE `jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `komputer`
---
-ALTER TABLE `komputer`
-  MODIFY `id_komputer` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `sessions`
---
-ALTER TABLE `sessions`
-  ADD CONSTRAINT `1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
